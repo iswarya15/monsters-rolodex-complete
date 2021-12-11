fc4424345f829471699c0182143efe4bc594918b
iswarya <iswarya.ch99@gmail.com>
2021-12-11 13:43:17 +0530

    Updates

---
+++ precache-manifest.31a6c0c42860fe65082e6054e4696512.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be48e5aa4be63b3a8b38378bc4b754b",
+    "url": "/monsters-rolodex-complete/index.html"
+  },
+  {
+    "revision": "0595083fdfc7ea7274a4",
+    "url": "/monsters-rolodex-complete/static/css/main.62d87302.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex-complete/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-complete/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0595083fdfc7ea7274a4",
+    "url": "/monsters-rolodex-complete/static/js/main.a343843c.chunk.js"
+  },
+  {
+    "revision": "4b601965c2149392e1f2",
+    "url": "/monsters-rolodex-complete/static/js/runtime-main.b3c2bfd0.js"
+  }
+]);
